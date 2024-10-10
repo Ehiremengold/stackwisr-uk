8c5583468fdafc75aaec88797d96ad4539818dcf
cxstackwisr <ehiremengold@gmail.com>
2024-10-10 11:57:34 +0100

    completed up to even page hero

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -1,10 +1,34 @@
 import "./Event.css";
 import EventHero from "./component/EventHero/EventHero";
+import { motion } from "framer-motion";
 
 const Event = () => {
   return (
     <>
-      <EventHero />
+      <motion.div
+        initial={{ opacity: 0, y: 50 }} // Starts off-screen and transparent
+        animate={{ opacity: 1, y: 0 }} // Moves into view and becomes fully visible
+        transition={{ duration: 0.8, ease: "easeOut", delay: 0.7 }} // Customize the speed and easing
+      >
+        <EventHero />
+      </motion.div>
+
+      <div className="event-body-title">
+        <div className="blue-title">
+          <div className="desc-bar"></div>
+          <h4>Our Events</h4>
+          <div className="desc-bar"></div>
+        </div>
+        <h1>Latest Events</h1>
+        <p className="hero-desc">
+          Get latest updates around our upcoming events and sign up to join for
+          free
+        </p>
+      </div>
+
+      <div className="event-body-section">
+        <div className="wrapper"></div>
+      </div>
     </>
   );
 };
--- src/pages/Event/component/EventHero/EventHero.jsx
+++ src/pages/Event/component/EventHero/EventHero.jsx
@@ -1,8 +1,24 @@
-import './EventHero.css'
+import "./EventHero.css";
+import eventsHero from "../../../../assets/images/eventshero.png";
 
 const EventHero = () => {
   return (
-    <div>EventHero</div>
-  )
-}
-export default EventHero
+    <section className="events">
+      <div className="wrapper">
+        <div className="events-title">
+          <h1>
+            UPCOMING <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;EVENTS
+          </h1>
+        </div>
+        <p>
+          Join our webinars and Expert Session training sessions to learn how to
+          build a real-life solution. We encourage you to register incase of
+          recordings to be sent, even if you can&apos;t attend live,
+        </p>
+        <img src={eventsHero} alt="" />
+      </div>
+    </section>
+  );
+};
+export default EventHero;
