137d9870ab99d4eeb092a950fb39e8f059ece844
cxstackwisr <ehiremengold@gmail.com>
2025-01-03 14:16:29 +0100

    refurbished Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,9 @@ import heroUser from "../../assets/images/heroo.png";
 import descImg from "../../assets/svg/top.svg";
 import arrowImg from "../../assets/icons/arrow.png";
 import planeImg from "../../assets/icons/plane.svg";
+import bookmarkPng from "../../assets/icons/bookmark.png";
+import starPng from "../../assets/icons/star.png";
+import userPng from "../../assets/icons/user.png";
 import { NavLink } from "react-router-dom";
 
 const Hero = ({ heroRef, heroIsInView }) => {
@@ -24,19 +27,33 @@ const Hero = ({ heroRef, heroIsInView }) => {
             </div>
 
             <div className="hero-text__container">
-              <h1>
-                Launch your career with a Professional Certificate from
-                StackwisR
-              </h1>
+              <h1>Welcome to StackwisR</h1>
               <p>
-              Secure your future with job-specific skills tailored for fields in the digital economy.
+                Secure your future with job-specific skills tailored for fields
+                in the digital economy. We offer on-demand career programs
+                designed to prepare you for pivotal career moments
               </p>
             </div>
 
+            <div className="we-offer-button-like">
+              <div className="button-like">
+                <img src={userPng} alt="" />
+                <p>2000+ Learners</p>
+              </div>
+              <div className="button-like">
+                <img src={bookmarkPng} alt="" />
+                <p>80% Completion</p>
+              </div>
+              <div className="button-like">
+                <img src={starPng} alt="" />
+                <p>4.7+ Rating</p>
+              </div>
+            </div>
+
             <div className="action-btns">
-              <a href="/careerpaths">
+              {/* <a href="/careerpaths">
                 <button>Get Started</button>
-              </a>
+              </a> */}
               <a href="/about-us">
                 <button className="learn-more-btn">Learn more</button>
               </a>
