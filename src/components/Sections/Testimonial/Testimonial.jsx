a61a77282037a57cd7df1332d02f80ee744b1731
cxstackwisr <ehiremengold@gmail.com>
2025-01-08 07:14:31 +0100

    testimony body on homepage page complete

--- src/components/Sections/Testimonial/Testimonial.jsx
+++ src/components/Sections/Testimonial/Testimonial.jsx
@@ -3,13 +3,14 @@ import testimonyBanner from "../../../assets/svg/real-story.svg";
 // import testifier from "../../../assets/images/testifier.jpg";
 // import testifier2 from "../../../assets/images/testifier2.png";
 import "./Testimonial.css";
-
+import { testimonies } from "./staticTestimonies";
+import stackwisrLogo from "../../../assets/logo/Ellipse.svg";
 const Testimonial = () => {
   return (
     <section className="testimonials">
       {/* <img src={pinkBg} className="testimonials-bg" alt="" /> */}
       <div className="wrapper">
-        <div className="section-text__header">
+        {/* <div className="section-text__header">
           <div className="main-desc">
             <div className="desc-bar"></div>
             <h4>Some Testimonies</h4>
@@ -17,21 +18,46 @@ const Testimonial = () => {
           </div>
           <h1>Testimonial</h1>
           <p>Let your success and journey be written in stones</p>
+        </div> */}
+
+        <div className="testimonial-body-grid">
+          <div className="testimonial-grid-boxx">
+            <h1>Our Testimonial</h1>
+            <p>
+              We have trained close to 3,000 delegates from across the world and
+              have received great testimonial about their success story.
+            </p>
+          </div>
+
+          <div className="frame-wrapper">
+            <iframe
+              loading="lazy"
+              width={350}
+              height={300}
+              allowfullscreen="allowfullscreen"
+              src="//www.youtube.com/embed/E-NKSnqU0Oc?rel=0&amp;autoplay=0&amp;controls=0"
+            ></iframe>
+          </div>
+
+          {testimonies.map(({ name, jobTitle, testimony }, index) => {
+            return (
+              <div className="box-replica testimonial-grid-boxx" key={index}>
+                <img
+                  src={stackwisrLogo}
+                  alt="stackwisr logo"
+                  className="logo"
+                />
+                <h2>{name}</h2>
+                <h5 style={{fontWeight:600}}>{jobTitle}</h5>
+                <h5>{testimony}</h5>
+              </div>
+            );
+          })}
         </div>
 
-        <div className="frame-wrapper">
-          <iframe
-            loading="lazy"
-            width={500}
-            height={300}
-            allowfullscreen="allowfullscreen"
-            src="//www.youtube.com/embed/E-NKSnqU0Oc?rel=0&amp;autoplay=0&amp;controls=0"
-          ></iframe>
-        </div>
-
-        <a href="/testimonies">
+        {/* <a href="/testimonies">
           <h3>See more &rarr;</h3>
-        </a>
+        </a> */}
       </div>
     </section>
   );
---
+++ src/components/Sections/Testimonial/staticTestimonies.js
@@ -0,0 +1,26 @@
+export const testimonies = [
+  {
+    name: "Stefano Porciello",
+    jobTitle: "Pricing and Revenue Management Specialist",
+    testimony:
+      "I am grateful to StackwisR for the incredible journey I started with them. I saw the effects of the Data Science programme quite immediately after completion. I secured an Analyst role with Hertz within a few months and then a Pricing Manager role at EMEA. I can definitely advise everyone interested in the Analytics field to enrol in their programmes.",
+  },
+  {
+    name: "Olayemi Olabisi",
+    jobTitle: "Business Intelligence Specialist",
+    testimony:
+      "After university, I got a graduate job and it wasn’t rewarding compared to the prospects StackwisR suggested to me for a career path in Analytics. After doing some research, I signed up for the courses, finished them, and got myself a new job in IT. It’s that simple and straightforward, to be honest. From my perspective it was perfect.",
+  },
+  {
+    name: "Tawseef Mushtaq",
+    jobTitle: "Data Science Consultant",
+    testimony:
+      "Credit goes to StackwisR. No doubt about that. At the core of my experience is the patience shown by the instructors in helping me gain the skills. There are many ingredients in this comprehensive program for people who want to switch careers or advance further. I got a way-better-than-expected role. I couldn’t have done so without StackwisR.",
+  },
+  {
+    name: "Joey Nanta",
+    jobTitle: "Data Science Specialist",
+    testimony:
+      "My career growth was getting a bit stunted and I set about looking for a change. StackwisR Data Science programme fit in perfectly with my future plans; and the content was exactly what I was looking for regarding industry relevance. I secured a senior analyst position with a reputable organisation and have since progressed further in my career.",
+  },
+];
