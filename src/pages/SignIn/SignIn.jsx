0ff48009509a1e4b94ae6f375a8f8302e961820b
cxstackwisr <ehiremengold@gmail.com>
2025-01-09 04:08:03 +0100

    MODIFYING sign in and sign up UIs

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,30 +1,41 @@
 import { Link } from "react-router-dom";
 import "./SignIn.css";
+import { Helmet } from "react-helmet";
 
 const SignIn = () => {
   return (
-    <section className="sign-in-body">
-      <div className="login-form-body">
-        <h2>SIGN IN</h2>
-        <form>
-          <div className="form-input-row">
-            <label htmlFor="email">Email</label>
-            <input type="email" />
-          </div>
+    <>
+      <Helmet>
+        <title>Sign in | Stackwisr</title>
+        <meta
+          name="description"
+          content="Sign in to Stackwisr to access expertly curated content from certified Stackwisr tutors and advance your career today."
+        ></meta>
+      </Helmet>
+      <section className="sign-in-body">
+        <div className="login-form-body">
+          <h2>SIGN IN</h2>
+          <form>
+            <div className="form-input-row">
+              <label htmlFor="email">Email</label>
+              <input type="email" />
+            </div>
 
-          <div className="form-input-row">
-            <label htmlFor="password">Password</label>
-            <input type="password" />
-          </div>
-          <button disabled className="form-submit-btn">
-            Login
-          </button>
-        </form>
-        <p>
-          Don&apos;t have an account? <Link to="/auth/create-account">Sign up</Link>
-        </p>
-      </div>
-    </section>
+            <div className="form-input-row">
+              <label htmlFor="password">Password</label>
+              <input type="password" />
+            </div>
+            <button disabled className="form-submit-btn">
+              <h3>LOGIN</h3>
+            </button>
+          </form>
+          <p>
+            Don&apos;t have an account?{" "}
+            <Link to="/auth/create-account">Sign up</Link>
+          </p>
+        </div>
+      </section>
+    </>
   );
 };
 export default SignIn;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,35 +1,59 @@
 import { Link } from "react-router-dom";
 import "./SignUp.css";
+import { Helmet } from "react-helmet";
 
 const SignUp = () => {
   return (
-    <section className="sign-up-body">
-      <div className="signup-form-body">
-        <h2>SIGN UP</h2>
-        <form>
-          <div className="form-input-row">
-            <label htmlFor="email">Email</label>
-            <input type="email" />
-          </div>
+    <>
+      <Helmet>
+        <title>Sign up | Stackwisr</title>
+        <meta
+          name="description"
+          content="Join Stackwisr today and gain access to curated resources, expert guidance, and personalized career pathways. Sign up now to start your journey!"
+        ></meta>
+      </Helmet>
+      <section className="sign-up-body">
+        <div className="signup-form-body">
+          <h2>SIGN UP</h2>
+          <form>
+            <div className="form-input-row">
+              <label htmlFor="email">Email</label>
+              <input type="email" />
+            </div>
 
-          <div className="form-input-row">
-            <label htmlFor="password">Password</label>
-            <input type="password" />
-          </div>
+            <div className="form-input-row">
+              <label htmlFor="password">Password</label>
+              <div className="input-password-validators">
+                <input type="password" />
+                <div className="password-validators">
+                  <ul>
+                    {[
+                      "Password length must be at least 8",
+                      "Password must not be common",
+                      "Password must not contain personal details",
+                      "Password should be alphanumeric",
+                    ].map((validator, index) => (
+                      <li key={index}>{validator}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
 
-          <div className="form-input-row">
-            <label htmlFor="password">Repeat Password</label>
-            <input type="password" />
-          </div>
-          <button disabled className="form-submit-btn">
-            Create Account
-          </button>
-        </form>
-        <p>
-          Already have an account? <Link to="/auth/login">Sign in</Link>
-        </p>
-      </div>
-    </section>
+            <div className="form-input-row">
+              <label htmlFor="password">Repeat Password</label>
+              <input type="password" />
+            </div>
+            <button disabled className="form-submit-btn">
+              <h3>CREATE ACCOUNT</h3>
+            </button>
+          </form>
+          <p>
+            Already have an account? <Link to="/auth/login">Sign in</Link>
+          </p>
+        </div>
+      </section>
+    </>
   );
 };
 export default SignUp;
